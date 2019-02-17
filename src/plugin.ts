177df4326446d363d4823786f1870c163bce6d7b
Stephen Haberman <stephen.haberman@gmail.com>
2019-02-16 20:44:07 -0600

    Write to stdout, but not hooked up yet.

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,8 +1,9 @@
 import { readToBuffer } from './utils';
 import { google } from '../build/pbjs';
-import CodeGeneratorRequest = google.protobuf.compiler.CodeGeneratorRequest;
 import { generateFile } from './main';
-import { createTypeMap } from "./types";
+import { createTypeMap } from './types';
+import CodeGeneratorRequest = google.protobuf.compiler.CodeGeneratorRequest;
+import CodeGeneratorResponse = google.protobuf.compiler.CodeGeneratorResponse;
 
 // this would be the plugin called by the protoc compiler
 async function main() {
@@ -14,5 +15,7 @@ async function main() {
   for (let file of request.protoFile) {
     generateFile(typeMap, file);
   }
-  // TODO write to stdout
+  const response = new CodeGeneratorResponse();
+  const buffer = CodeGeneratorResponse.encode(response).finish();
+  process.stdout.write(Buffer.from(buffer));
 }
