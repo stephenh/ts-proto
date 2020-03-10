28335f64f170e313a595e3209c8b2de40dec46ba
Stephen Haberman <stephen.haberman@gmail.com>
2020-03-10 09:28:36 -0500

    Comments.

--- pbjs.sh
+++ pbjs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Generates JS/TS objects with pbjs for the protoc descriptors which ts-proto
+# Generates JS/TS objects of the protoc plugin descriptors with pbjs, which ts-proto
 # uses to understand the incoming protoc codegen request objects.
 #
-# I.e. currently the shipped plugin uses pbjs-generated types of the protobuf DTOs
-# at _build-time_ to enerate its output ts-proto types. At somepoint we could become
-# self-hosted, i.e. use our own ts-proto-generated types, which would be spiffy but
-# not a big deal.
+# I.e. currently the shipped ts-proto plugin uses pbjs-generated types of the protobuf DTOs
+# at build-time to generate the requested ts-proto types. At somepoint we could become
+# self-hosted, i.e. use our own ts-proto-generated types within the plugin itself, which
+# would be spiffy but not a big deal.
 
 mkdir -p build
 yarn pbjs --force-message -t static-module -o build/pbjs.js \
