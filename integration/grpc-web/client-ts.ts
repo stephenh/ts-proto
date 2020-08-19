d0d6a4052e3cda2f28d988bd33109ce853ae0f16
Stephen Haberman <stephen.haberman@gmail.com>
2020-08-18 21:04:46 -0500

    Update client-ts.

--- integration/grpc-web/client-ts.ts
+++ integration/grpc-web/client-ts.ts
@@ -1,19 +1,33 @@
 #!/usr/bin/env node
 
 import { NodeHttpTransport } from '@improbable-eng/grpc-web-node-http-transport';
-import { DashStateClientImpl, GrpcWebImpl } from './example';
+import { DashAPICredsClientImpl, DashStateClientImpl, GrpcWebImpl } from './example';
 
 const rpc = new GrpcWebImpl('http://localhost:9090', {
   transport: NodeHttpTransport(),
   debug: false,
 });
+
 const client = new DashStateClientImpl(rpc);
+const creds = new DashAPICredsClientImpl(rpc);
 
 async function main() {
+  console.log('calling client.UserSettings');
   console.log(await client.UserSettings({}));
+
+  console.log('calling creds.Create');
+  const cred = await creds.Create({ description: 'test desc fooo', metadata: 'test metadata' });
+  console.log(cred);
+
+  console.log('calling creds.Delete');
+  const del = await creds.Delete({
+    id: cred.id,
+    credSid: '...not included in original example...',
+  });
+  console.log(del);
 }
 
 main().then(
   () => console.log('done'),
-  (err) => console.log(err)
+  (err) => console.log('failed', err)
 );
