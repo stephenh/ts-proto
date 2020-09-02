7ffc119b9a1ccd50f4839985d264358c94be3d60
Myles Scolnick <mscolnick@gmail.com>
2020-09-01 17:54:27 -0700

    Fix toJson for maps of objects

--- integration/batching-with-context/batching.ts
+++ integration/batching-with-context/batching.ts
@@ -402,7 +402,7 @@ export const BatchMapQueryResponse = {
     obj.entities = {};
     if (message.entities) {
       Object.entries(message.entities).forEach(([k, v]) => {
-        obj.entities[k] = v;
+        obj.entities[k] = Entity.fromJSON(v);
       })
     }
     return obj;
--- integration/simple-snake/simple.ts
+++ integration/simple-snake/simple.ts
@@ -1223,7 +1223,7 @@ export const SimpleWithMap = {
     obj.entitiesById = {};
     if (message.entitiesById) {
       Object.entries(message.entitiesById).forEach(([k, v]) => {
-        obj.entitiesById[k] = v;
+        obj.entitiesById[k] = Entity.fromJSON(v);
       })
     }
     obj.nameLookup = {};
@@ -1485,7 +1485,7 @@ export const SimpleWithSnakeCaseMap = {
     obj.entities_by_id = {};
     if (message.entities_by_id) {
       Object.entries(message.entities_by_id).forEach(([k, v]) => {
-        obj.entities_by_id[k] = v;
+        obj.entities_by_id[k] = Entity.fromJSON(v);
       })
     }
     return obj;
