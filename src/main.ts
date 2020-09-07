a1b73bc9df782109ab14f10c49b56dccd0b963e3
Timo Stamm <ts@timostamm.de>
2020-09-07 11:53:58 +0200

    Casting Long as any when comparing to protobuf.util.Long for configureProtobuf.
    This commit changes the emitted snipped from:
    ```
    import * as protobufjs from 'protobufjs/minimal';
    if (protobuf.util.Long !== Long) {
      protobuf.util.Long = Long;
      protobuf.configure();
    }
    ```
    
    to:
    ```
    import { util, configure } from 'protobufjs/minimal';
    if (util.Long !== Long as any) {
      util.Long = Long as any;
      configure();
    }
    ```
    
    As suggested by @Raiondesu in this comment:
    https://github.com/stephenh/ts-proto/pull/125#issuecomment-688138919

--- src/main.ts
+++ src/main.ts
@@ -260,11 +260,11 @@ function addConfigureProtobuf(file: FileSpec, options: Options): FileSpec {
     return file;
   }
   return file.addCode(
-    CodeBlock.empty()
-      .beginControlFlow('if (%T.util.Long !== %T)', 'protobuf*protobufjs/minimal', 'Long*long')
-      .addStatement('%T.util.Long = %T', 'protobuf*protobufjs/minimal', 'Long*long')
-      .addStatement('%T.configure()', 'protobuf*protobufjs/minimal')
-      .endControlFlow()
+      CodeBlock.empty()
+          .beginControlFlow('if (%T.Long !== %T as any)', 'util@protobufjs/minimal', 'Long*long')
+          .addStatement('%T.Long = %T as any', 'util@protobufjs/minimal', 'Long*long')
+          .addStatement('%T()', 'configure@protobufjs/minimal')
+          .endControlFlow()
   );
 }
 
