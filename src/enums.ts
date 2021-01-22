49cb35edf912dd8bc33bad28886fd91ea9d646ea
Stephen Haberman <stephen.haberman@gmail.com>
2021-01-21 21:47:49 -0600

    Extract enums.ts file.

---
+++ src/enums.ts
@@ -0,0 +1,99 @@
+import { Options } from './main';
+import { google } from '../build/pbjs';
+import { code, Code, joinCode } from 'ts-poet';
+import { maybeAddComment } from './utils';
+import EnumDescriptorProto = google.protobuf.EnumDescriptorProto;
+import { camelCase } from './case';
+import SourceInfo, { Fields } from './sourceInfo';
+
+const UNRECOGNIZED_ENUM_NAME = 'UNRECOGNIZED';
+const UNRECOGNIZED_ENUM_VALUE = -1;
+
+// Output the `enum { Foo, A = 0, B = 1 }`
+export function generateEnum(
+  options: Options,
+  fullName: string,
+  enumDesc: EnumDescriptorProto,
+  sourceInfo: SourceInfo
+): Code {
+  const chunks: Code[] = [];
+
+  maybeAddComment(sourceInfo, chunks, enumDesc.options?.deprecated);
+  chunks.push(code`export enum ${fullName} {`);
+
+  enumDesc.value.forEach((valueDesc, index) => {
+    const info = sourceInfo.lookup(Fields.enum.value, index);
+    maybeAddComment(info, chunks, valueDesc.options?.deprecated, `${valueDesc.name} - `);
+    chunks.push(
+      code`${valueDesc.name} = ${options.stringEnums ? `"${valueDesc.name}"` : valueDesc.number.toString()},`
+    );
+  });
+
+  if (options.addUnrecognizedEnum)
+    chunks.push(code`
+      ${UNRECOGNIZED_ENUM_NAME} = ${
+      options.stringEnums ? `"${UNRECOGNIZED_ENUM_NAME}"` : UNRECOGNIZED_ENUM_VALUE.toString()
+    },`);
+  chunks.push(code`}`);
+
+  if (options.outputJsonMethods) {
+    chunks.push(code`\n`);
+    chunks.push(generateEnumFromJson(fullName, enumDesc, options));
+    chunks.push(code`\n`);
+    chunks.push(generateEnumToJson(fullName, enumDesc));
+  }
+
+  return joinCode(chunks, { on: '\n' });
+}
+
+/** Generates a function with a big switch statement to decode JSON -> our enum. */
+export function generateEnumFromJson(fullName: string, enumDesc: EnumDescriptorProto, options: Options): Code {
+  const chunks: Code[] = [];
+
+  chunks.push(code`export function ${camelCase(fullName)}FromJSON(object: any): ${fullName} {`);
+  chunks.push(code`switch (object) {`);
+
+  for (const valueDesc of enumDesc.value) {
+    chunks.push(code`
+      case ${valueDesc.number}:
+      case "${valueDesc.name}":
+        return ${fullName}.${valueDesc.name};
+    `);
+  }
+
+  if (options.addUnrecognizedEnum) {
+    chunks.push(code`
+      case ${UNRECOGNIZED_ENUM_VALUE}:
+      case "${UNRECOGNIZED_ENUM_NAME}":
+      default:
+        return ${fullName}.${UNRECOGNIZED_ENUM_NAME};
+    `);
+  } else {
+    // We use globalThis to avoid conflicts on protobuf types named `Error`.
+    chunks.push(code`
+      default:
+        throw new globalThis.Error("Unrecognized enum value " + object + " for enum ${fullName}");
+    `);
+  }
+
+  chunks.push(code`}`);
+  chunks.push(code`}`);
+  return joinCode(chunks, { on: '\n' });
+}
+
+/** Generates a function with a big switch statement to encode our enum -> JSON. */
+export function generateEnumToJson(fullName: string, enumDesc: EnumDescriptorProto): Code {
+  const chunks: Code[] = [];
+
+  chunks.push(code`export function ${camelCase(fullName)}ToJSON(object: ${fullName}): string {`);
+  chunks.push(code`switch (object) {`);
+
+  for (const valueDesc of enumDesc.value) {
+    chunks.push(code`case ${fullName}.${valueDesc.name}: return "${valueDesc.name}";`);
+  }
+  chunks.push(code`default: return "UNKNOWN";`);
+
+  chunks.push(code`}`);
+  chunks.push(code`}`);
+  return joinCode(chunks, { on: '\n' });
+}
--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ import {
 } from './types';
 import SourceInfo, { Fields } from './sourceInfo';
 import { maybeAddComment, optionsFromParameter } from './utils';
-import { camelCase, camelToSnake, capitalize, maybeSnakeToCamel } from './case';
+import { camelToSnake, capitalize, maybeSnakeToCamel } from './case';
 import {
   generateNestjsGrpcServiceMethodsDecorator,
   generateNestjsServiceClient,
@@ -52,6 +52,7 @@ import FileDescriptorProto = google.protobuf.FileDescriptorProto;
 import EnumDescriptorProto = google.protobuf.EnumDescriptorProto;
 import ServiceDescriptorProto = google.protobuf.ServiceDescriptorProto;
 import { ConditionalOutput } from 'ts-poet/build/ConditionalOutput';
+import { generateEnum } from './enums';
 
 export enum LongOption {
   NUMBER = 'number',
@@ -400,95 +401,6 @@ function makeTimestampMethods(options: Options, longs: ReturnType<typeof makeLon
   );
 
   return { toTimestamp, fromTimestamp, fromJsonTimestamp };
-}
-
-const UNRECOGNIZED_ENUM_NAME = 'UNRECOGNIZED';
-const UNRECOGNIZED_ENUM_VALUE = -1;
-
-// TODO move to enums.ts
-
-// Output the `enum { Foo, A = 0, B = 1 }`
-function generateEnum(options: Options, fullName: string, enumDesc: EnumDescriptorProto, sourceInfo: SourceInfo): Code {
-  const chunks: Code[] = [];
-
-  maybeAddComment(sourceInfo, chunks, enumDesc.options?.deprecated);
-  chunks.push(code`export enum ${fullName} {`);
-
-  enumDesc.value.forEach((valueDesc, index) => {
-    const info = sourceInfo.lookup(Fields.enum.value, index);
-    maybeAddComment(info, chunks, valueDesc.options?.deprecated, `${valueDesc.name} - `);
-    chunks.push(
-      code`${valueDesc.name} = ${options.stringEnums ? `"${valueDesc.name}"` : valueDesc.number.toString()},`
-    );
-  });
-
-  if (options.addUnrecognizedEnum)
-    chunks.push(code`
-      ${UNRECOGNIZED_ENUM_NAME} = ${
-      options.stringEnums ? `"${UNRECOGNIZED_ENUM_NAME}"` : UNRECOGNIZED_ENUM_VALUE.toString()
-    },`);
-  chunks.push(code`}`);
-
-  if (options.outputJsonMethods) {
-    chunks.push(code`\n`);
-    chunks.push(generateEnumFromJson(fullName, enumDesc, options));
-    chunks.push(code`\n`);
-    chunks.push(generateEnumToJson(fullName, enumDesc));
-  }
-
-  return joinCode(chunks, { on: '\n' });
-}
-
-/** Generates a function with a big switch statement to decode JSON -> our enum. */
-function generateEnumFromJson(fullName: string, enumDesc: EnumDescriptorProto, options: Options): Code {
-  const chunks: Code[] = [];
-
-  chunks.push(code`export function ${camelCase(fullName)}FromJSON(object: any): ${fullName} {`);
-  chunks.push(code`switch (object) {`);
-
-  for (const valueDesc of enumDesc.value) {
-    chunks.push(code`
-      case ${valueDesc.number}:
-      case "${valueDesc.name}":
-        return ${fullName}.${valueDesc.name};
-    `);
-  }
-
-  if (options.addUnrecognizedEnum) {
-    chunks.push(code`
-      case ${UNRECOGNIZED_ENUM_VALUE}:
-      case "${UNRECOGNIZED_ENUM_NAME}":
-      default:
-        return ${fullName}.${UNRECOGNIZED_ENUM_NAME};
-    `);
-  } else {
-    // We use globalThis to avoid conflicts on protobuf types named `Error`.
-    chunks.push(code`
-      default:
-        throw new globalThis.Error("Unrecognized enum value " + object + " for enum ${fullName}");
-    `);
-  }
-
-  chunks.push(code`}`);
-  chunks.push(code`}`);
-  return joinCode(chunks, { on: '\n' });
-}
-
-/** Generates a function with a big switch statement to encode our enum -> JSON. */
-function generateEnumToJson(fullName: string, enumDesc: EnumDescriptorProto): Code {
-  const chunks: Code[] = [];
-
-  chunks.push(code`export function ${camelCase(fullName)}ToJSON(object: ${fullName}): string {`);
-  chunks.push(code`switch (object) {`);
-
-  for (const valueDesc of enumDesc.value) {
-    chunks.push(code`case ${fullName}.${valueDesc.name}: return "${valueDesc.name}";`);
-  }
-  chunks.push(code`default: return "UNKNOWN";`);
-
-  chunks.push(code`}`);
-  chunks.push(code`}`);
-  return joinCode(chunks, { on: '\n' });
 }
 
 // When useOptionals=true, non-scalar fields are translated into optional properties.
