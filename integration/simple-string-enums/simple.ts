8901dbdcb269f086de6579cfecdbf8db7c68212a
Stephen Haberman <stephen.haberman@gmail.com>
2021-02-01 08:54:07 -0600

    Update simple-string-enums to repro.

--- integration/simple-string-enums/simple.ts
+++ integration/simple-string-enums/simple.ts
@@ -8,7 +8,90 @@ export enum StateEnum {
   UNRECOGNIZED = 'UNRECOGNIZED',
 }
 
+export function stateEnumFromJSON(object: any): StateEnum {
+  switch (object) {
+    case 0:
+    case 'UNKNOWN':
+      return StateEnum.UNKNOWN;
+    case 2:
+    case 'ON':
+      return StateEnum.ON;
+    case 3:
+    case 'OFF':
+      return StateEnum.OFF;
+    case -1:
+    case 'UNRECOGNIZED':
+    default:
+      return StateEnum.UNRECOGNIZED;
+  }
+}
+
+export function stateEnumToJSON(object: StateEnum): string {
+  switch (object) {
+    case StateEnum.UNKNOWN:
+      return 'UNKNOWN';
+    case StateEnum.ON:
+      return 'ON';
+    case StateEnum.OFF:
+      return 'OFF';
+    default:
+      return 'UNKNOWN';
+  }
+}
+
 export interface Simple {
   name: string;
   state: StateEnum;
 }
+
+const baseSimple: object = { name: '', state: 0 };
+
+export const Simple = {
+  fromJSON(object: any): Simple {
+    const message = { ...baseSimple } as Simple;
+    if (object.name !== undefined && object.name !== null) {
+      message.name = String(object.name);
+    } else {
+      message.name = '';
+    }
+    if (object.state !== undefined && object.state !== null) {
+      message.state = stateEnumFromJSON(object.state);
+    } else {
+      message.state = 0;
+    }
+    return message;
+  },
+
+  fromPartial(object: DeepPartial<Simple>): Simple {
+    const message = { ...baseSimple } as Simple;
+    if (object.name !== undefined && object.name !== null) {
+      message.name = object.name;
+    } else {
+      message.name = '';
+    }
+    if (object.state !== undefined && object.state !== null) {
+      message.state = object.state;
+    } else {
+      message.state = 0;
+    }
+    return message;
+  },
+
+  toJSON(message: Simple): unknown {
+    const obj: any = {};
+    message.name !== undefined && (obj.name = message.name);
+    message.state !== undefined && (obj.state = stateEnumToJSON(message.state));
+    return obj;
+  },
+};
+
+type Builtin = Date | Function | Uint8Array | string | number | undefined;
+export type DeepPartial<T> = T extends Builtin
+  ? T
+  : T extends Array<infer U>
+  ? Array<DeepPartial<U>>
+  : T extends ReadonlyArray<infer U>
+  ? ReadonlyArray<DeepPartial<U>>
+  : T extends {}
+  ? { [K in keyof T]?: DeepPartial<T[K]> }
+  : Partial<T>;
