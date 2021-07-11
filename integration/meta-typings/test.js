45744fd79302c510e10c5c0c7a32bebab8f278f1
Stephen Haberman <stephen.haberman@gmail.com>
2021-07-11 17:38:51 -0500

    Remove js files.

--- integration/meta-typings/test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var simple_1 = require("./simple");
-var fs_1 = require("fs");
-console.log(simple_1.protoMetadata);
-fs_1.writeFileSync('./out', JSON.stringify(simple_1.protoMetadata, undefined, 2));
