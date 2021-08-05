fab354f3f8f3aae51bb0377f98138bc80a1113e3
Aleksei Androsov <aandrosov@yandex-team.ru>
2021-08-05 22:06:55 +0300

    fix: Field starting with '_' generates an interface property starting with 'undefined' (#344)

--- tests/case-test.ts
+++ tests/case-test.ts
@@ -20,4 +20,20 @@ describe('case', () => {
   it('does nothing is already camel', () => {
     expect(maybeSnakeToCamel('FooBar', { snakeToCamel: true })).toEqual('FooBar');
   });
+
+  // deal with original protoc which converts
+  // _uuid -> Uuid
+  // __uuid -> Uuid
+  // _uuid_foo -> UuidFoo
+  it('converts snake to camel with first underscore', () => {
+    expect(maybeSnakeToCamel('_uuid', { snakeToCamel: true })).toEqual('Uuid');
+  });
+
+  it('converts snake to camel with first double underscore', () => {
+    expect(maybeSnakeToCamel('__uuid', { snakeToCamel: true })).toEqual('Uuid');
+  });
+
+  it('converts snake to camel with first underscore and camelize other', () => {
+    expect(maybeSnakeToCamel('_uuid_foo', { snakeToCamel: true })).toEqual('UuidFoo');
+  });
 });
