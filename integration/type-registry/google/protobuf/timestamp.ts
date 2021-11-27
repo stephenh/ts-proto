b3e44149cf904e220be0f6e178210f442bddd3d8
Bouke Versteegh <info@boukeversteegh.nl>
2021-11-27 12:37:18 +0100

    Recompile ts without docker

--- integration/type-registry/google/protobuf/timestamp.ts
+++ integration/type-registry/google/protobuf/timestamp.ts
@@ -201,3 +201,12 @@ if (util.Long !== Long) {
   util.Long = Long as any;
   configure();
 }
+rn long.toNumber();
+}
+
+// If you get a compile-error about 'Constructor<Long> and ... have no overlap',
+// add '--ts_proto_opt=esModuleInterop=true' as a flag when calling 'protoc'.
+if (util.Long !== Long) {
+  util.Long = Long as any;
+  configure();
+}
