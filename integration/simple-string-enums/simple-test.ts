2a07a6a7424aefefd9c7b6305bb9b9f6b5dd4111
Bouke Versteegh <info@boukeversteegh.nl>
2021-12-02 10:17:06 +0100

    Move test for string enum nullvalue to simple-string-enums

--- integration/simple-string-enums/simple-test.ts
+++ integration/simple-string-enums/simple-test.ts
@@ -1,13 +1,15 @@
 import { Simple, StateEnum } from './simple';
+import { NullValue } from "./google/protobuf/struct";
 
 describe('simple-string-enums', () => {
   it('encodes', () => {
-    const s1: Simple = { name: 'a', state: StateEnum.ON, states: [StateEnum.ON, StateEnum.OFF] };
+    const s1: Simple = { name: 'a', state: StateEnum.ON, states: [StateEnum.ON, StateEnum.OFF], nullValue: NullValue.NULL_VALUE };
     const b = Simple.encode(s1).finish();
     const s2 = Simple.decode(b);
     expect(s2).toMatchInlineSnapshot(`
       Object {
         "name": "a",
+        "nullValue": "NULL_VALUE",
         "state": "ON",
         "states": Array [
           "ON",
