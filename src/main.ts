2d7a5d04c72ace58fa3a6745c3857f6cc0468543
Bouke Versteegh <info@boukeversteegh.nl>
2021-12-08 15:22:52 +0000

    fix: noImplicitReturns error in Value.unwrap (#436)
    * fix noImplicitReturns error in Value.unwrap
    
    Fixes #432
    
    * Force shell script LF line-endings
    
    * Update generated struct.ts files
    
    * Ensure parameters.txt are checked out with LF line endings
    
    * Ensure protoc-gen-dump is checked out with LF line endings

--- src/main.ts
+++ src/main.ts
@@ -1370,19 +1370,20 @@ function generateUnwrap(fullProtoTypeName: string): Code[] {
 
   if (isAnyValueTypeName(fullProtoTypeName)) {
     chunks.push(code`unwrap(message: Value): string | number | boolean | Object | null | Array<any> | undefined {
-      if (message.stringValue !== undefined) {
+      if (message?.stringValue !== undefined) {
         return message.stringValue;
-      } else if (message.numberValue !== undefined) {
+      } else if (message?.numberValue !== undefined) {
         return message.numberValue;
-      } else if (message.boolValue !== undefined) {
+      } else if (message?.boolValue !== undefined) {
         return message.boolValue;
-      } else if (message.structValue !== undefined) {
+      } else if (message?.structValue !== undefined) {
         return message.structValue;
-      } else if (message.listValue !== undefined) {
+      } else if (message?.listValue !== undefined) {
           return message.listValue;
-      } else if (message.nullValue !== undefined) {
+      } else if (message?.nullValue !== undefined) {
         return null;
       }
+      return undefined;
     }`);
   }
 
