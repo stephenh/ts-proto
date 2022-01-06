0ec5e53f23cae26ef96ac90a2c9ca33c87b400e4
AlaaZorkane <alaazorkane@gmail.com>
2022-01-07 00:34:48 +0100

    feat: delimited-methods stream encoding/decoding tests

--- integration/delimited-methods/delimited-methods-test.ts
+++ integration/delimited-methods/delimited-methods-test.ts
@@ -1,14 +1,32 @@
-import { Simple } from './delimited-methods';
+import { Reader, Writer } from 'protobufjs';
+import { AnotherSimple, Simple } from './delimited-methods';
 
 describe('delimited-methods', () => {
-  const message: Simple = {
+  // normal size
+  const messageA = Simple.fromPartial({
     age: 42,
     name: 'John Doe',
     children: ['Jane', 'Jack', 'Joe'],
-  };
+  });
+
+  // big size
+  const messageB = Simple.fromPartial({
+    age: 2147483647, // max int32
+    name: 'A Very Long Name',
+    children: ['Jane', 'Jack', 'Joe', 'Jill', 'Jane Jr.', 'Jack Jr.', 'Joe Jr.', 'Jill Jr.'],
+  });
+
+  // minimum size
+  const messageC = Simple.fromPartial({});
+
+  // different message type
+  const messageD = AnotherSimple.fromPartial({
+    num: 2147483.75,
+    str: 'A Very Long Name',
+  });
 
   it('encodes with a length delimiter', () => {
-    const encoded = Simple.encodeDelimited(message).finish();
+    const encoded = Simple.encodeDelimited(messageA).finish();
 
     // -1 for the length delimiter
     const length = encoded.length - 1;
@@ -17,10 +35,54 @@ describe('delimited-methods', () => {
     expect(length).toEqual(delimiter);
   });
 
-  it('decodes length-delimited messages', () => {
-    const encoded = Simple.encodeDelimited(message).finish();
+  it('decodes a length-delimited message', () => {
+    const encoded = Simple.encodeDelimited(messageA).finish();
     const decoded = Simple.decodeDelimited(encoded);
 
-    expect(decoded).toEqual(message);
+    expect(decoded).toEqual(messageA);
+  });
+
+  it('decodes a stream of same length-delimited messages', () => {
+    const writer = new Writer();
+    const messages = [messageA, messageB, messageC, messageB];
+
+    messages.forEach((msg) => Simple.encodeDelimited(msg, writer));
+
+    const stream = writer.finish();
+
+    const reader = new Reader(stream);
+
+    const decodedMessages = messages.map(() => Simple.decodeDelimited(reader));
+
+    expect(decodedMessages).toEqual(messages);
+  });
+
+  /**
+   * Requires knowing the type order of messages beforehand
+   *
+   * Could also be done programmatically by checking for unique properties
+   */
+  it('decodes a stream of different length-delimited messages', () => {
+    const writer = new Writer();
+
+    const messages = [messageA, messageD, messageC, messageD] as const;
+
+    Simple.encodeDelimited(messages[0], writer);
+    AnotherSimple.encodeDelimited(messages[1], writer);
+    Simple.encodeDelimited(messages[2], writer);
+    AnotherSimple.encodeDelimited(messages[3], writer);
+
+    const stream = writer.finish();
+
+    const reader = new Reader(stream);
+
+    const decodedMessages = [
+      Simple.decodeDelimited(reader),
+      AnotherSimple.decodeDelimited(reader),
+      Simple.decodeDelimited(reader),
+      AnotherSimple.decodeDelimited(reader),
+    ];
+
+    expect(decodedMessages).toEqual(messages);
   });
 });
