320c6de0378d685a4af9cbe1adf79030973992ff
Robin Bartholdson <robin.bartholdson@formulate.app>
2022-02-14 21:16:34 +0100

    rm pb.js-dependency, make sub-tests self-contained

--- integration/fieldmask/fieldmask-test.ts
+++ integration/fieldmask/fieldmask-test.ts
@@ -1,18 +1,13 @@
-import { Reader } from 'protobufjs';
 import { FieldMaskMessage } from './fieldmask';
 
-import { FieldMaskMessage as PbFieldMaskMessage } from './pbjs';
-
 let data = {
-  fieldMask: "a,b,c.d",
+  fieldMask: 'a,b,c.d',
 };
 
 describe('fieldmask', () => {
-  it('can encode objects', () => {
-    const s1 = FieldMaskMessage.fromJSON(data);
-    const s2 = PbFieldMaskMessage.decode(Reader.create(FieldMaskMessage.encode(s1).finish()));
-
-    expect(s2).toMatchInlineSnapshot(`
+  it('can decode JSON', () => {
+    const f = FieldMaskMessage.fromJSON(data);
+    expect(f).toMatchInlineSnapshot(`
       Object {
         "fieldMask": Object {
           "paths": Array [
@@ -25,11 +20,8 @@ describe('fieldmask', () => {
     `);
   });
 
-  it('can decode objects', () => {
-    let message = FieldMaskMessage.fromJSON(data);
-    let encodedValue = FieldMaskMessage.encode(message).finish();
-    const decodedValue = FieldMaskMessage.decode(Reader.create(encodedValue));
-
-    expect(FieldMaskMessage.toJSON(decodedValue)).toEqual(data);
+  it('can encode JSON', () => {
+    const f = FieldMaskMessage.toJSON({ fieldMask: { paths: ['a', 'b', 'c.d'] } });
+    expect(f).toEqual(data);
   });
 });
