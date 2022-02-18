d07001ef810cc64228aa2ca8e8486a54df1066c4
Kyle Maxwell <kyle@kylemaxwell.com>
2022-02-18 12:44:34 -0800

    Add M1/ARM support for the test suite

--- aliases.sh
+++ aliases.sh
@@ -3,7 +3,14 @@ PROJECT_ROOT=$(realpath $(dirname "$BASH_SOURCE"))
 PROJECT_ROOT_DOCKER="//ts-proto" # double slash to support git bash on windows
 
 # Alias docker-compose to make it usable from anywhere.
-function _docker-compose() { docker-compose -f $PROJECT_ROOT/docker-compose.yml "$@"; }
+function _docker-compose() { 
+  if [ uname -a | grep arm64 ]
+  then
+    docker-compose -f "$PROJECT_ROOT/docker-compose.aarch64.yml" "$@"
+  else
+    docker-compose -f $PROJECT_ROOT/docker-compose.yml "$@"; 
+  fi
+}
 
 # Dockerized version of protoc.
 function protoc() { _docker-compose run --rm -w //host --entrypoint protoc -- protoc "$@"; }
