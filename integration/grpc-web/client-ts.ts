e40ce67f5fa4cd6735b26d7d1dedde3d621256dc
luhuaei <luhuaei@gmail.com>
2022-07-12 15:44:13 +0800

    grpc-web: fix undefine message

--- integration/grpc-web/client-ts.ts
+++ integration/grpc-web/client-ts.ts
@@ -31,7 +31,7 @@ async function main() {
   try {
     await creds.Update({ description: 'test desc2' });
   } catch (e) {
-    console.log('got expected error', e.message);
+    console.log('got expected error', e);
   }
 
   const obs = client.ActiveUserSettingsStream({});
