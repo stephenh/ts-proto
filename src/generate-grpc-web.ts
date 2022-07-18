1167c30c363213159b904ef143dabcd183b16ab9
Francois HERSENT <francois.hersent@hiventive.com>
2022-07-18 17:37:03 +0200

    feat: add dynamic upStreamCodes option

--- src/generate-grpc-web.ts
+++ src/generate-grpc-web.ts
@@ -195,6 +195,7 @@ function generateGrpcWebImpl(ctx: Context, returnObservable: boolean, hasStreami
       ${hasStreamingMethods ? 'streamingTransport?: grpc.TransportFactory,' : ``}
       debug?: boolean,
       metadata?: grpc.Metadata,
+      upStreamRetryCodes?: number[],
     }
   `;
 
@@ -300,7 +301,7 @@ function createInvokeMethod(ctx: Context) {
       metadata: grpc.Metadata | undefined
     ): ${observableType(ctx)}<any> {
       // Status Response Codes (https://developers.google.com/maps-booking/reference/grpc-api/status_codes)
-      const upStreamCodes = [2, 4, 8, 9, 10, 13, 14, 15]; 
+      const upStreamCodes = this.options.upStreamRetryCodes || [2, 4, 8, 9, 10, 13, 14, 15];
       const DEFAULT_TIMEOUT_TIME: number = 3_000;
       const request = { ..._request, ...methodDesc.requestType };
       const maybeCombinedMetadata =
