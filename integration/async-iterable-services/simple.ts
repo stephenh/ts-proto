fff39b258f97036dc07c3545190ed2bf16f9a85b
Christian Stewart <christian@paral.in>
2022-07-27 20:08:49 -0700

    chore(integration): add regression test for #628
    Signed-off-by: Christian Stewart <christian@paral.in>

--- integration/async-iterable-services/simple.ts
+++ integration/async-iterable-services/simple.ts
@@ -3,31 +3,32 @@ import * as _m0 from 'protobufjs/minimal';
 
 export const protobufPackage = 'simple';
 
-export interface TestMessage {
-  value: string;
+/** EchoMsg is the message body for Echo. */
+export interface EchoMsg {
+  body: string;
 }
 
-function createBaseTestMessage(): TestMessage {
-  return { value: '' };
+function createBaseEchoMsg(): EchoMsg {
+  return { body: '' };
 }
 
-export const TestMessage = {
-  encode(message: TestMessage, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
-    if (message.value !== '') {
-      writer.uint32(10).string(message.value);
+export const EchoMsg = {
+  encode(message: EchoMsg, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+    if (message.body !== '') {
+      writer.uint32(10).string(message.body);
     }
     return writer;
   },
 
-  decode(input: _m0.Reader | Uint8Array, length?: number): TestMessage {
+  decode(input: _m0.Reader | Uint8Array, length?: number): EchoMsg {
     const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
     let end = length === undefined ? reader.len : reader.pos + length;
-    const message = createBaseTestMessage();
+    const message = createBaseEchoMsg();
     while (reader.pos < end) {
       const tag = reader.uint32();
       switch (tag >>> 3) {
         case 1:
-          message.value = reader.string();
+          message.body = reader.string();
           break;
         default:
           reader.skipType(tag & 7);
@@ -38,70 +39,99 @@ export const TestMessage = {
   },
 
   // encodeTransform encodes a source of message objects.
-  // Transform<TestMessage, Uint8Array>
+  // Transform<EchoMsg, Uint8Array>
   async *encodeTransform(
-    source: AsyncIterable<TestMessage | TestMessage[]> | Iterable<TestMessage | TestMessage[]>
+    source: AsyncIterable<EchoMsg | EchoMsg[]> | Iterable<EchoMsg | EchoMsg[]>
   ): AsyncIterable<Uint8Array> {
     for await (const pkt of source) {
       if (Array.isArray(pkt)) {
         for (const p of pkt) {
-          yield* [TestMessage.encode(p).finish()];
+          yield* [EchoMsg.encode(p).finish()];
         }
       } else {
-        yield* [TestMessage.encode(pkt).finish()];
+        yield* [EchoMsg.encode(pkt).finish()];
       }
     }
   },
 
   // decodeTransform decodes a source of encoded messages.
-  // Transform<Uint8Array, TestMessage>
+  // Transform<Uint8Array, EchoMsg>
   async *decodeTransform(
     source: AsyncIterable<Uint8Array | Uint8Array[]> | Iterable<Uint8Array | Uint8Array[]>
-  ): AsyncIterable<TestMessage> {
+  ): AsyncIterable<EchoMsg> {
     for await (const pkt of source) {
       if (Array.isArray(pkt)) {
         for (const p of pkt) {
-          yield* [TestMessage.decode(p)];
+          yield* [EchoMsg.decode(p)];
         }
       } else {
-        yield* [TestMessage.decode(pkt)];
+        yield* [EchoMsg.decode(pkt)];
       }
     }
   },
 
-  fromJSON(object: any): TestMessage {
+  fromJSON(object: any): EchoMsg {
     return {
-      value: isSet(object.value) ? String(object.value) : '',
+      body: isSet(object.body) ? String(object.body) : '',
     };
   },
 
-  toJSON(message: TestMessage): unknown {
+  toJSON(message: EchoMsg): unknown {
     const obj: any = {};
-    message.value !== undefined && (obj.value = message.value);
+    message.body !== undefined && (obj.body = message.body);
     return obj;
   },
 
-  fromPartial<I extends Exact<DeepPartial<TestMessage>, I>>(object: I): TestMessage {
-    const message = createBaseTestMessage();
-    message.value = object.value ?? '';
+  fromPartial<I extends Exact<DeepPartial<EchoMsg>, I>>(object: I): EchoMsg {
+    const message = createBaseEchoMsg();
+    message.body = object.body ?? '';
     return message;
   },
 };
 
-export interface Test {
-  BidiStreaming(request: AsyncIterable<TestMessage>): AsyncIterable<TestMessage>;
+/** Echoer service returns the given message. */
+export interface Echoer {
+  /** Echo returns the given message. */
+  Echo(request: EchoMsg): Promise<EchoMsg>;
+  /** EchoServerStream is an example of a server -> client one-way stream. */
+  EchoServerStream(request: EchoMsg): AsyncIterable<EchoMsg>;
+  /** EchoClientStream is an example of client->server one-way stream. */
+  EchoClientStream(request: AsyncIterable<EchoMsg>): Promise<EchoMsg>;
+  /** EchoBidiStream is an example of a two-way stream. */
+  EchoBidiStream(request: AsyncIterable<EchoMsg>): AsyncIterable<EchoMsg>;
 }
 
-export class TestClientImpl implements Test {
+export class EchoerClientImpl implements Echoer {
   private readonly rpc: Rpc;
   constructor(rpc: Rpc) {
     this.rpc = rpc;
-    this.BidiStreaming = this.BidiStreaming.bind(this);
+    this.Echo = this.Echo.bind(this);
+    this.EchoServerStream = this.EchoServerStream.bind(this);
+    this.EchoClientStream = this.EchoClientStream.bind(this);
+    this.EchoBidiStream = this.EchoBidiStream.bind(this);
   }
-  BidiStreaming(request: AsyncIterable<TestMessage>): AsyncIterable<TestMessage> {
-    const data = TestMessage.encodeTransform(request);
-    const result = this.rpc.bidirectionalStreamingRequest('simple.Test', 'BidiStreaming', data);
-    return TestMessage.decodeTransform(result);
+  Echo(request: EchoMsg): Promise<EchoMsg> {
+    const data = EchoMsg.encode(request).finish();
+    const promise = this.rpc.request('simple.Echoer', 'Echo', data);
+    return promise.then((data) => EchoMsg.decode(new _m0.Reader(data)));
+  }
+
+  EchoServerStream(request: EchoMsg): AsyncIterable<EchoMsg> {
+    const data = EchoMsg.encode(request).finish();
+    const result = this.rpc.serverStreamingRequest('simple.Echoer', 'EchoServerStream', data);
+    return EchoMsg.decodeTransform(result);
+  }
+
+  EchoClientStream(request: AsyncIterable<EchoMsg>): Promise<EchoMsg> {
+    const data = EchoMsg.encodeTransform(request);
+    const promise = this.rpc.clientStreamingRequest('simple.Echoer', 'EchoClientStream', data);
+    return promise.then((data) => EchoMsg.decode(new _m0.Reader(data)));
+  }
+
+  EchoBidiStream(request: AsyncIterable<EchoMsg>): AsyncIterable<EchoMsg> {
+    const data = EchoMsg.encodeTransform(request);
+    const result = this.rpc.bidirectionalStreamingRequest('simple.Echoer', 'EchoBidiStream', data);
+    return EchoMsg.decodeTransform(result);
   }
 }
 
