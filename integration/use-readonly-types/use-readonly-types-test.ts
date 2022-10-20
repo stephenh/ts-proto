a6e63679e8723b03edf98ea899186bdcfd79460a
Jonas Kello <jonas.kello@gmail.com>
2022-10-20 19:45:46 +0200

    Add more fields

--- integration/use-readonly-types/use-readonly-types-test.ts
+++ integration/use-readonly-types/use-readonly-types-test.ts
@@ -3,7 +3,12 @@ import { Entity } from "./use-readonly-types";
 describe("use-readonly-types", () => {
   it("generates types correctly", () => {
     const m: Entity = {
-      id: 42,
+      intVal: 42,
+      intArray: [42, 43],
+      stringVal: "hello",
+      stringArray: ["hello", "world"],
+      subEntity: { subVal: 42 },
+      subEntityArray: [{ subVal: 42 }, { subVal: 43 }],
     };
     const jsonFromObject = Entity.toJSON(m);
     const entityFromJSON = Entity.fromJSON(jsonFromObject);
@@ -11,15 +16,30 @@ describe("use-readonly-types", () => {
     const encoded = Entity.encode(m).finish();
     const decoded = Entity.decode(encoded);
     expect(decoded).toEqual({
-      id: m.id,
+      intVal: m.intVal,
+      intArray: m.intArray,
+      stringVal: m.stringVal,
+      stringArray: m.stringArray,
+      subEntity: m.subEntity,
+      subEntityArray: m.subEntityArray,
     });
     const jsonFromDecoded = Entity.toJSON(decoded);
     expect(jsonFromDecoded).toEqual(jsonFromObject);
     const partial = Entity.fromPartial({
-      id: m.id,
+      intVal: m.intVal,
+      intArray: m.intArray,
+      stringVal: m.stringVal,
+      stringArray: m.stringArray,
+      subEntity: m.subEntity,
+      subEntityArray: m.subEntityArray,
     });
     expect(partial).toEqual({
-      id: m.id,
+      intVal: m.intVal,
+      intArray: m.intArray,
+      stringVal: m.stringVal,
+      stringArray: m.stringArray,
+      subEntity: m.subEntity,
+      subEntityArray: m.subEntityArray,
     });
   });
 });
--- integration/use-readonly-types/use-readonly-types.ts
+++ integration/use-readonly-types/use-readonly-types.ts
@@ -4,17 +4,43 @@ import * as _m0 from "protobufjs/minimal";
 export const protobufPackage = "";
 
 export interface Entity {
-  readonly id: number;
+  readonly intVal: number;
+  readonly stringVal: string;
+  readonly intArray: number[];
+  readonly stringArray: string[];
+  readonly subEntity: SubEntity | undefined;
+  readonly subEntityArray: SubEntity[];
+}
+
+export interface SubEntity {
+  readonly subVal: number;
 }
 
 function createBaseEntity(): Entity {
-  return { id: 0 };
+  return { intVal: 0, stringVal: "", intArray: [], stringArray: [], subEntity: undefined, subEntityArray: [] };
 }
 
 export const Entity = {
   encode(message: Entity, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
-    if (message.id !== 0) {
-      writer.uint32(8).int32(message.id);
+    if (message.intVal !== 0) {
+      writer.uint32(8).int32(message.intVal);
+    }
+    if (message.stringVal !== "") {
+      writer.uint32(18).string(message.stringVal);
+    }
+    writer.uint32(26).fork();
+    for (const v of message.intArray) {
+      writer.int32(v);
+    }
+    writer.ldelim();
+    for (const v of message.stringArray) {
+      writer.uint32(34).string(v!);
+    }
+    if (message.subEntity !== undefined) {
+      SubEntity.encode(message.subEntity, writer.uint32(42).fork()).ldelim();
+    }
+    for (const v of message.subEntityArray) {
+      SubEntity.encode(v!, writer.uint32(50).fork()).ldelim();
     }
     return writer;
   },
@@ -27,7 +53,29 @@ export const Entity = {
       const tag = reader.uint32();
       switch (tag >>> 3) {
         case 1:
-          message.id = reader.int32();
+          message.intVal = reader.int32();
+          break;
+        case 2:
+          message.stringVal = reader.string();
+          break;
+        case 3:
+          if ((tag & 7) === 2) {
+            const end2 = reader.uint32() + reader.pos;
+            while (reader.pos < end2) {
+              message.intArray.push(reader.int32());
+            }
+          } else {
+            message.intArray.push(reader.int32());
+          }
+          break;
+        case 4:
+          message.stringArray.push(reader.string());
+          break;
+        case 5:
+          message.subEntity = SubEntity.decode(reader, reader.uint32());
+          break;
+        case 6:
+          message.subEntityArray.push(SubEntity.decode(reader, reader.uint32()));
           break;
         default:
           reader.skipType(tag & 7);
@@ -38,18 +86,99 @@ export const Entity = {
   },
 
   fromJSON(object: any): Entity {
-    return { id: isSet(object.id) ? Number(object.id) : 0 };
+    return {
+      intVal: isSet(object.intVal) ? Number(object.intVal) : 0,
+      stringVal: isSet(object.stringVal) ? String(object.stringVal) : "",
+      intArray: Array.isArray(object?.intArray) ? object.intArray.map((e: any) => Number(e)) : [],
+      stringArray: Array.isArray(object?.stringArray) ? object.stringArray.map((e: any) => String(e)) : [],
+      subEntity: isSet(object.subEntity) ? SubEntity.fromJSON(object.subEntity) : undefined,
+      subEntityArray: Array.isArray(object?.subEntityArray)
+        ? object.subEntityArray.map((e: any) => SubEntity.fromJSON(e))
+        : [],
+    };
   },
 
   toJSON(message: Entity): unknown {
     const obj: any = {};
-    message.id !== undefined && (obj.id = Math.round(message.id));
+    message.intVal !== undefined && (obj.intVal = Math.round(message.intVal));
+    message.stringVal !== undefined && (obj.stringVal = message.stringVal);
+    if (message.intArray) {
+      obj.intArray = message.intArray.map((e) => Math.round(e));
+    } else {
+      obj.intArray = [];
+    }
+    if (message.stringArray) {
+      obj.stringArray = message.stringArray.map((e) => e);
+    } else {
+      obj.stringArray = [];
+    }
+    message.subEntity !== undefined &&
+      (obj.subEntity = message.subEntity ? SubEntity.toJSON(message.subEntity) : undefined);
+    if (message.subEntityArray) {
+      obj.subEntityArray = message.subEntityArray.map((e) => e ? SubEntity.toJSON(e) : undefined);
+    } else {
+      obj.subEntityArray = [];
+    }
     return obj;
   },
 
   fromPartial<I extends Exact<DeepPartial<Entity>, I>>(object: I): Entity {
     const message = createBaseEntity() as any;
-    message.id = object.id ?? 0;
+    message.intVal = object.intVal ?? 0;
+    message.stringVal = object.stringVal ?? "";
+    message.intArray = object.intArray?.map((e) => e) || [];
+    message.stringArray = object.stringArray?.map((e) => e) || [];
+    message.subEntity = (object.subEntity !== undefined && object.subEntity !== null)
+      ? SubEntity.fromPartial(object.subEntity)
+      : undefined;
+    message.subEntityArray = object.subEntityArray?.map((e) => SubEntity.fromPartial(e)) || [];
+    return message;
+  },
+};
+
+function createBaseSubEntity(): SubEntity {
+  return { subVal: 0 };
+}
+
+export const SubEntity = {
+  encode(message: SubEntity, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
+    if (message.subVal !== 0) {
+      writer.uint32(8).int32(message.subVal);
+    }
+    return writer;
+  },
+
+  decode(input: _m0.Reader | Uint8Array, length?: number): SubEntity {
+    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = createBaseSubEntity() as any;
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.subVal = reader.int32();
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): SubEntity {
+    return { subVal: isSet(object.subVal) ? Number(object.subVal) : 0 };
+  },
+
+  toJSON(message: SubEntity): unknown {
+    const obj: any = {};
+    message.subVal !== undefined && (obj.subVal = Math.round(message.subVal));
+    return obj;
+  },
+
+  fromPartial<I extends Exact<DeepPartial<SubEntity>, I>>(object: I): SubEntity {
+    const message = createBaseSubEntity() as any;
+    message.subVal = object.subVal ?? 0;
     return message;
   },
 };
