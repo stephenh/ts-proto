863096407cc6547d8dbf90ee5bf6d11d4e213cef
Stephen Haberman <stephen.haberman@gmail.com>
2022-11-13 08:35:20 -0600

    Run proto2bin.

--- integration/nice-grpc/google/protobuf/empty.ts
+++ integration/nice-grpc/google/protobuf/empty.ts
@@ -11,6 +11,8 @@ export const protobufPackage = "google.protobuf";
  *     service Foo {
  *       rpc Bar(google.protobuf.Empty) returns (google.protobuf.Empty);
  *     }
+ *
+ * The JSON representation for `Empty` is empty JSON object `{}`.
  */
 export interface Empty {
 }
--- integration/only-types/google/protobuf/any.ts
+++ integration/only-types/google/protobuf/any.ts
@@ -28,7 +28,7 @@ export const protobufPackage = "google.protobuf";
  *       foo = any.unpack(Foo.class);
  *     }
  *
- * Example 3: Pack and unpack a message in Python.
+ *  Example 3: Pack and unpack a message in Python.
  *
  *     foo = Foo(...)
  *     any = Any()
@@ -38,7 +38,7 @@ export const protobufPackage = "google.protobuf";
  *       any.Unpack(foo)
  *       ...
  *
- * Example 4: Pack and unpack a message in Go
+ *  Example 4: Pack and unpack a message in Go
  *
  *      foo := &pb.Foo{...}
  *      any, err := anypb.New(foo)
@@ -58,7 +58,7 @@ export const protobufPackage = "google.protobuf";
  * name "y.z".
  *
  * JSON
- *
+ * ====
  * The JSON representation of an `Any` value uses the regular
  * representation of the deserialized, embedded message, with an
  * additional field `@type` which contains the type URL. Example:
