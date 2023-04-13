3b697f4ebb46b3c89a3e6c4696587d2829eb1528
mo4islona <mo4islona@gmail.com>
2023-04-13 04:36:05 +0400

    fix: null check in .google.protobuf.Timestamp wrapper

--- src/main.ts
+++ src/main.ts
@@ -419,12 +419,16 @@ function makeProtobufTimestampWrapper() {
   return code`
       ${wrappers}['.google.protobuf.Timestamp'] = {
         fromObject(value: Date) {
+          if (!(date instanceof Date)) return null;
+              
           return {
             seconds: value.getTime() / 1000,
             nanos: (value.getTime() % 1000) * 1e6,
           };
         },
         toObject(message: { seconds: number; nanos: number }) {
+          if(!message) return null;
+          
           return new Date(message.seconds * 1000 + message.nanos / 1e6);
         },
       } as any;`;
