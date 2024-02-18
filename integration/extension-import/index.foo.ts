c5c67b786b918503afe14a06230ebb1b76be5bbe
Jonas Kello <jonas.kello@gmail.com>
2024-02-18 13:53:41 +0100

    fix: Use as any on globalThis.Buffer check for bytesFromBase64

---
+++ integration/extension-import/index.foo.ts
@@ -0,0 +1,4 @@
+/* eslint-disable */
+
+export * from "./base";
+export * from "./extension";
--- integration/simple-prototype-defaults/simple.ts
+++ integration/simple-prototype-defaults/simple.ts
@@ -2913,7 +2913,7 @@ interface Rpc {
 }
 
 function bytesFromBase64(b64: string): Uint8Array {
-  if (globalThis.Buffer) {
+  if ((globalThis as any).Buffer) {
     return Uint8Array.from(globalThis.Buffer.from(b64, "base64"));
   } else {
     const bin = globalThis.atob(b64);
