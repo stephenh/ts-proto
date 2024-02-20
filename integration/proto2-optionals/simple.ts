20ec61bcc5f21ec078f21445314fde7f4327474d
Luke Alvoeiro <lukejalvoeiro@gmail.com>
2024-02-19 18:25:05 -0800

    test cases

---
+++ integration/proto2-optionals/simple.ts
@@ -0,0 +1,34 @@
+/* eslint-disable */
+
+export const protobufPackage = "simple";
+
+export interface Simple {
+  name: string;
+  age?:
+    | number
+    | undefined;
+  /** @deprecated */
+  oldAddress?: Simple_OldAddress | undefined;
+  newAddress?: Simple_NewAddress | undefined;
+  email?: string | undefined;
+  hasLoggedInRecently?: boolean | undefined;
+  profilePic?: Uint8Array | undefined;
+  luckyNumbers: number[];
+  properties: { [key: string]: string };
+}
+
+export interface Simple_OldAddress {
+  street?: string | undefined;
+  city: string;
+}
+
+export interface Simple_NewAddress {
+  street: string;
+  city: string;
+  country: string;
+}
+
+export interface Simple_PropertiesEntry {
+  key: string;
+  value: string;
+}
