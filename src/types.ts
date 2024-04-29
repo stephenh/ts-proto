05c399f454c5afa4bcc7d4fa898aa8ba49c075db
Daniel Santiago <dasco144@hotmail.com>
2024-04-29 21:14:37 +0200

    add comments

--- src/types.ts
+++ src/types.ts
@@ -203,6 +203,11 @@ export function getFieldOptionsJsType(
       return FieldDescriptorProto_Type.TYPE_STRING;
     case FieldOptions_JSType.JS_NUMBER:
       return FieldDescriptorProto_Type.TYPE_INT64;
+    // In the case of JS_NORMAL, we don't want to override the type, so we return
+    case FieldOptions_JSType.JS_NORMAL:
+    // In the case of UNRECOGNIZED, we assume defualt behavior and we don't want to override the type, so we return
+    case FieldOptions_JSType.UNRECOGNIZED:
+      return;
   }
 }
 
