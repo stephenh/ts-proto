9c5b588fef354d41d474c416d580e71351a9c864
Luke Alvoeiro <lukejalvoeiro@gmail.com>
2024-05-14 16:02:18 -0700

    make test better

--- integration/options-types-only/options-test.ts
+++ integration/options-types-only/options-test.ts
@@ -4,4 +4,8 @@ describe("options", () => {
   it("compiles", () => {
     expect(protoMetadata).not.toBeUndefined();
   });
+
+  it("has the right options", () => {
+    expect(protoMetadata.options?.messages?.["MyMessage"]?.options?.["my_message_option"]).toBe(1234);
+  });
 });
