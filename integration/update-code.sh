e2c149e9b85b5717945df045af836ac97f1cb1d4
Stephen Haberman <stephen.haberman@gmail.com>
2024-06-04 21:54:11 -0500

    chore: Remove the bin testing approach (#1050)

---
+++ integration/update-code.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+if [[ "$OSTYPE" == "msys" ]]; then
+  PLUGIN_PATH="../protoc-gen-ts_proto.bat"
+else
+  PLUGIN_PATH="../protoc-gen-ts_proto"
+fi
+
+if [[ $# -eq 0 ]]; then
+  TESTS=$(find . -mindepth 1 -maxdepth 1 -type d)
+else
+  TESTS=$@
+fi
+
+for TEST in $TESTS; do
+  echo "Test ${TEST}"
+  cd "${TEST}"
+
+  PARAMETERS_FILE="parameters.txt"
+  if [ -f "$PARAMETERS_FILE" ]; then
+    PARAMETERS=$(cat "$PARAMETERS_FILE")
+  else
+    PARAMETERS=""
+  fi
+
+  NODE_OPTIONS="--import tsx" protoc --experimental_allow_proto3_optional \
+    "--plugin=$PLUGIN_PATH" \
+    --ts_proto_opt="annotateFilesWithVersion=false,${PARAMETERS}" \
+    --ts_proto_out=./ \
+    ./*.proto
+
+  echo ""
+  echo ""
+
+  cd ..
+done
