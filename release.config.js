069a87455b039ed11edd09ddd623af8044ab82a0
Stephen Haberman <stephen.haberman@gmail.com>
2024-08-15 17:31:28 -0500

    chore: Bump semantic-release, use conventionalcommits.

--- release.config.js
+++ release.config.js
@@ -4,7 +4,7 @@ module.exports = {
     { name: 'alpha', prerelease: true },
   ],
   plugins: [
-    '@semantic-release/commit-analyzer',
+    ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@semantic-release/npm',
