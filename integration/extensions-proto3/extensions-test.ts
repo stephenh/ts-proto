a474ee5e6066f5727898476fc406106da05960d4
Zak Henry <zak@cloudnc.com>
2024-11-11 09:46:26 +1300

    feat(Test): add sample for testing proto3 extensions

---
+++ integration/extensions-proto3/extensions-test.ts
@@ -0,0 +1,8 @@
+describe("extensions-proto3-test", () => {
+
+  it("generates readable extension values for services")
+  it("generates readable extension values for methods")
+  it("generates readable extension values for messages")
+  it("generates readable extension values for fields")
+
+});
