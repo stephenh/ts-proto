2cdd59480b8d00405679f131ceb5a2c0aba4151a
Stephen Haberman <stephen.haberman@gmail.com>
2025-12-20 14:19:13 -0600

    chore: Use new output format.

--- release.config.js
+++ release.config.js
@@ -1,15 +1,13 @@
 module.exports = {
-  branches: [
-    'main',
-    { name: 'alpha', prerelease: true },
-  ],
+  branches: ["main", { name: "alpha", prerelease: true }],
+  repositoryUrl: "https://github.com/stephenh/ts-proto.git",
   plugins: [
-    ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
-    '@semantic-release/github',
-    '@semantic-release/git',
-    ['@semantic-release/exec', { publishCmd: 'echo "::set-output name=release_version::${nextRelease.version}"' }],
+    ["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    "@semantic-release/git",
+    ["@semantic-release/exec", { publishCmd: 'echo "release_version=${nextRelease.version}" >> $GITHUB_OUTPUT' }],
   ],
 };
