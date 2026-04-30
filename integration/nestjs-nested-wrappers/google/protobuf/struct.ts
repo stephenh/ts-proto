e78e63abf1f12c73ec6bc4f13f3d1af171de9a4a
gnazarov <gnazarov16@gmail.com>
2026-05-01 00:08:33 +0200

    regen struct and timestamp with no comments

--- integration/nestjs-nested-wrappers/google/protobuf/struct.ts
+++ integration/nestjs-nested-wrappers/google/protobuf/struct.ts
@@ -6,30 +6,12 @@ import { wrappers } from "protobufjs";
 
 export const protobufPackage = "google.protobuf";
 
-/**
- * `NullValue` is a singleton enumeration to represent the null value for the
- * `Value` type union.
- *
- * The JSON representation for `NullValue` is JSON `null`.
- */
 export enum NullValue {
-  /** NULL_VALUE - Null value. */
   NULL_VALUE = 0,
   UNRECOGNIZED = -1,
 }
 
-/**
- * `Struct` represents a structured data value, consisting of fields
- * which map to dynamically typed values. In some languages, `Struct`
- * might be supported by a native representation. For example, in
- * scripting languages like JS a struct is represented as an
- * object. The details of that representation are described together
- * with the proto support for the language.
- *
- * The JSON representation for `Struct` is JSON object.
- */
 export interface Struct {
-  /** Unordered map of dynamically typed values. */
   fields: { [key: string]: any | undefined };
 }
 
@@ -38,46 +20,16 @@ export interface Struct_FieldsEntry {
   value: any | undefined;
 }
 
-/**
- * `Value` represents a dynamically typed value which can be either
- * null, a number, a string, a boolean, a recursive struct value, or a
- * list of values. A producer of value is expected to set one of these
- * variants. Absence of any variant indicates an error.
- *
- * The JSON representation for `Value` is JSON value.
- */
 export interface Value {
-  /** Represents a null value. */
-  nullValue?:
-    | NullValue
-    | undefined;
-  /** Represents a double value. */
-  numberValue?:
-    | number
-    | undefined;
-  /** Represents a string value. */
-  stringValue?:
-    | string
-    | undefined;
-  /** Represents a boolean value. */
-  boolValue?:
-    | boolean
-    | undefined;
-  /** Represents a structured value. */
-  structValue?:
-    | { [key: string]: any }
-    | undefined;
-  /** Represents a repeated `Value`. */
+  nullValue?: NullValue | undefined;
+  numberValue?: number | undefined;
+  stringValue?: string | undefined;
+  boolValue?: boolean | undefined;
+  structValue?: { [key: string]: any } | undefined;
   listValue?: Array<any> | undefined;
 }
 
-/**
- * `ListValue` is a wrapper around a repeated field of values.
- *
- * The JSON representation for `ListValue` is JSON array.
- */
 export interface ListValue {
-  /** Repeated field of dynamically typed values. */
   values: any[];
 }
 
